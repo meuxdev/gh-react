0b29fd6ecbe0898fd4eb5c556745efdd6130165e
Alejandro Andrade <meuxdev@gmail.com>
2022-07-23 23:05:33 -0500

    FIX base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://meuxdev.github.io/gh-react',
+  base: 'https://meuxdev.github.io/gh-react/',
   plugins: [react()]
 })
